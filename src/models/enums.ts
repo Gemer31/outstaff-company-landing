04635cde3e8dc0355f20ba231da659d3bce35ed6
rakr0819 <raman.krauchanka@netcracker.com>
2024-12-01 22:08:14 +0300

    process

--- src/models/enums.ts
+++ src/models/enums.ts
@@ -1,70 +1,70 @@
 export enum RouterLinks {
-    HOME = '/',
-    CONTACTS = '/contacts',
-    VACANCIES = "/vacancies",
-    EDITOR = "/editor",
-    SIGN_IN = "/signIn",
-    NOT_FOUND = "/not-found"
+  HOME = '/',
+  CONTACTS = '/contacts',
+  VACANCIES = '/vacancies',
+  EDITOR = '/editor',
+  SIGN_IN = '/signIn',
+  NOT_FOUND = '/not-found'
 }
 
 export enum DomIds {
-    NOTIFICATION = 'notification',
-    CONTACT_US_POPUP_ID = 'contactUsPopupId',
-    POPUP_ID = 'popupId',
-    POPUP_TITLE = "popupTitle",
-    POPUP_CONTENT = "popupContent",
+  NOTIFICATION = 'notification',
+  CONTACT_US_POPUP_ID = 'contactUsPopupId',
+  POPUP_ID = 'popupId',
+  POPUP_TITLE = 'popupTitle',
+  POPUP_CONTENT = 'popupContent',
 }
 
 export enum ContactLinkType {
-    PHONE = "phone",
-    MAIL = "mail",
-    TELEGRAM = "telegram"
+  PHONE = 'phone',
+  MAIL = 'mail',
+  TELEGRAM = 'telegram'
 }
 
 export enum FirestoreCollections {
-    SETTINGS = 'settings',
-    COUNTER_BLOCKS = 'counterBlocks',
-    CUSTOMERS_BLOCK = 'customersBlock',
-    VACANCIES = 'vacancies',
+  SETTINGS = 'settings',
+  COUNTER_BLOCKS = 'counterBlocks',
+  CUSTOMERS_BLOCK = 'customersBlock',
+  VACANCIES = 'vacancies',
 }
 
 export enum ButtonColorOptions {
-    RED = 'red',
-    GRAY = 'gray',
+  RED = 'red',
+  GRAY = 'gray',
 }
 
 export enum ButtonTypes {
-    BUTTON = 'button',
-    SUBMIT = 'submit',
+  BUTTON = 'button',
+  SUBMIT = 'submit',
 }
 
 export enum EditGroup {
-    GENERAL = 'general',
-    COUNTER_BLOCKS = 'counterBlocks',
-    VACANCIES = 'vacancies',
-    CUSTOMERS_BLOCK = 'customersBlock',
-    IMAGES = 'images',
+  GENERAL = 'general',
+  COUNTER_BLOCKS = 'counterBlocks',
+  VACANCIES = 'vacancies',
+  CUSTOMERS_BLOCK = 'customersBlock',
+  IMAGES = 'images',
 }
 
 export enum FirestoreDocuments {
-    CONFIG = 'config',
+  CONFIG = 'config',
 }
 
 export enum JobType {
-    DEVELOPER = "developer",
-    QA = "qa",
-    TECHNICAL_ARCHITECT = "architect",
-    PRODUCT_MANAGER = "manager",
-    ANALYST = "analyst",
-    DESIGNER = "designer",
+  DEVELOPER = 'developer',
+  QA = 'qa',
+  TECHNICAL_ARCHITECT = 'architect',
+  PRODUCT_MANAGER = 'manager',
+  ANALYST = 'analyst',
+  DESIGNER = 'designer',
 }
 
 export enum JobSchedule {
-    REMOTE = "remote",
-    OFFICE = "office",
+  REMOTE = 'remote',
+  OFFICE = 'office',
 }
 
 export enum SpecializationCardSize {
-    LARGE = 'large',
-    NORMAL = 'normal',
+  LARGE = 'large',
+  NORMAL = 'normal',
 }
